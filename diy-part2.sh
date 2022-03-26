b7907fbcdad59237eadece7e99e8fb2f10680ca7
YinGL <71273896+yglyjc@users.noreply.github.com>
2022-03-26 20:53:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.8.9/g' package/base-files/files/bin/config_generate
+# Modify the theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+
